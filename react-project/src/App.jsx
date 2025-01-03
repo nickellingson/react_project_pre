c3e1fdea67651116300a970ba4c1fba6098bffe5
Nicholas Ellingson <nickellingson8@gmail.com>
2025-01-02 20:18:03 -0600

    start of project

---
+++ react-project/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import MovieCard from './components/MovieCard'
+
+function App() {
+
+  return (
+    <MovieCard movie = {{title: "Movie Title", release_date: "2025"}}/>
+  )
+}
+
+
+
+
+export default App
